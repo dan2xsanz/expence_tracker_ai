0a512746e5fc076553a4d6c4a73c2e8098ad5ece
danlessanz <danles.sanz@gmail.com>
2025-04-27 23:48:27 +0800

    Added settings buttons

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -30,59 +30,88 @@ export default function SettingScreen() {
           style={{ fontSize: 12, marginTop: 5, marginBottom: 10 }}
           label={"Personal Details (Click profile to edit details)"}
         />
-        <View
-          style={{
-            marginTop: 10,
-            width: "100%",
-            height: "auto",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity>
-            <Image
-              source={require("../../assets/face.jpg")}
-              style={{
-                width: 80,
-                height: 80,
-                borderRadius: 100,
-                backgroundColor: "#fafafa",
-                borderColor: "#1db11da9",
-                borderWidth: 3.5,
-              }}
-            />
-          </TouchableOpacity>
-          <Label
-            label={`${accountDetail.firstName} ${accountDetail.lastName}`}
-            style={{ fontWeight: 500, fontSize: 20 }}
-          />
-          <Label
-            label={`1234567890`}
-            style={{ color: "#2522227a", fontSize: 12 }}
-          />
-        </View>
-        <Label
-          size={"small"}
-          style={{ fontSize: 12, marginTop: 5, marginBottom: 10 }}
-          label={"Security"}
-        />
-        <View
+
+        <TouchableOpacity
           style={{
             flexDirection: "row",
+            gap: 15,
             alignItems: "center",
-            justifyContent: "space-between",
+            marginTop: 10,
           }}
         >
-          <Label
-            label={`${accountDetail.email.substring(0, 6)}${"*".repeat(
-              accountDetail.email.length - 6
-            )}`}
-            style={{ color: "#2522227a", fontSize: 13 }}
+          <Image
+            source={require("../../assets/face.jpg")}
+            style={{
+              width: 90,
+              height: 90,
+              borderRadius: 100,
+              backgroundColor: "#fafafa",
+              borderColor: "#1db11da9",
+              borderWidth: 3.5,
+            }}
           />
-          <Label
-            label={`***************`}
-            style={{ color: "#2522227a", fontSize: 13 }}
-          />
-        </View>
+          <View>
+            <Label
+              label={`${accountDetail.firstName} ${accountDetail.lastName}`}
+              style={{ fontWeight: 600, fontSize: 25 }}
+            />
+            <Label
+              label={`1234567890`}
+              style={{ color: "#2522227a", fontSize: 12 }}
+            />
+            <Label
+              label={`${accountDetail.email.substring(0, 6)}${"*".repeat(
+                accountDetail.email.length - 6
+              )}`}
+              style={{ color: "#2522227a", fontSize: 13 }}
+            />
+          </View>
+        </TouchableOpacity>
+        <Label
+          size={"small"}
+          style={{ fontSize: 12, marginTop: 20, marginBottom: 10 }}
+          label={"Settings"}
+        />
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Currency Type"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Saved Transactions"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Recurring Transactions"} />
+        </TouchableOpacity>
+        <Label
+          size={"small"}
+          style={{ fontSize: 12, marginTop: 20 }}
+          label={"Support"}
+        />
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Help"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Send us message"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Privacy Policy"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Data Sharing Agreement"} />
+        </TouchableOpacity>
+        <Label
+          size={"small"}
+          style={{ fontSize: 12, marginTop: 20, marginBottom: 10 }}
+          label={"Security"}
+        />
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Change Password"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Switch Account"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={setting_style.button_style}>
+          <Label style={{ fontSize: 16 }} label={"Logout"} />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -105,5 +134,12 @@ const setting_style = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  button_style: {
+    width: "100%",
+    height: 45,
+    justifyContent: "center",
+    borderBottomWidth: 2,
+    borderColor: "#024738ff",
   },
 });
